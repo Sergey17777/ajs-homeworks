ab1e06c6228febb5683365fdb1723a6eeac65da9
Сергей Григорьев <92942323+Sergey17777@users.noreply.github.com>
2022-05-18 18:57:17 +0500

    Add files via upload

---
+++ AJS_lesson1/dist/app.js
@@ -0,0 +1,23 @@
+"use strict";
+
+require("core-js/modules/es.array.filter.js");
+
+require("core-js/modules/es.object.to-string.js");
+
+var characters = [{
+  name: 'мечник',
+  health: 10
+}, {
+  name: 'маг',
+  health: 100
+}, {
+  name: 'маг',
+  health: 0
+}, {
+  name: 'лучник',
+  health: 0
+}];
+var alive = characters.filter(function (item) {
+  return item.health > 0;
+});
+alive(characters);
---
+++ AJS_lesson1/src/js/app.js
@@ -0,0 +1,6 @@
+console.log('app wworked');
+
+import {Game, GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame} from './game';
+
+const game = new Game();
+game.start();
